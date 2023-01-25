4a8c14f7974f100b557059ba7526ddf8c151755c
Kit <nikitabit@yandex.ru>
2023-01-25 12:28:36 +0700

    Add check_redirect()

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 import requests
+from requests.exceptions import HTTPError
+
 import os
 
-for id in range(1,11):
-    dir = os.makedirs('books', exist_ok=True)
-    url = f"https://tululu.org/txt.php?id={id}"
 
-    filename = f"book-{id}.txt"
-    response = requests.get(url)
-    response.raise_for_status()
-    print(filename)
+def main():
+    for id in range(1,11):
+        dir = os.makedirs('books', exist_ok=True)
+        url = f"https://tululu.org/txt.php?id={id}"
 
-    with open(f"books/{filename}", 'wb') as book:
-        book.write(response.content)
+        filename = f"book-{id}.txt"
+        response = requests.get(url, allow_redirects=False)
+        response.raise_for_status()
+        try:
+            check_redirect(response)
+        except:
+            print(f"Книги c id {id} не существует.")
+            continue
+
+        with open(f"books/{filename}", 'wb') as book:
+            book.write(response.content)
+        print(f"{filename} is downloaded")
+
+
+def check_redirect(response):
+    if response.status_code !=200:
+        raise HTTPError()
+
+
+
+
+
+if __name__ == "__main__":
+    main()
