de6d630abc70684f64cecdd17154b499b81ae0d3
Kit <nikitabit@yandex.ru>
2023-01-26 10:17:08 +0700

    Download comments.

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ from urllib.parse import urljoin
 
 BOOK_FOLDER="books/"
 IMAGE_FOLDER="images/"
+COMMENTS_FOLDER="comments/"
 
 def main():
 
     os.makedirs(BOOK_FOLDER, exist_ok=True)
     os.makedirs(IMAGE_FOLDER, exist_ok=True)
+    os.makedirs(COMMENTS_FOLDER, exist_ok=True)
 
     for id in range(1,11):
         book_url = f"https://tululu.org/b{id}/"
@@ -24,15 +26,18 @@ def main():
             print(f'Книги c id "{id}" не существует.')
             continue
 
-        title, author, image_link = get_book_context(head_response)
+        title, author, image_link, comments = get_book_context(head_response)
+
         book_filename = f"{id}. {title}.txt"
         book_save_path = download_txt(download_response, book_filename, BOOK_FOLDER)
 
         image_filename = Path(image_link).name
         full_img_link = urljoin(book_url, image_link)
 
-        book_image_save_path = download_image(full_img_link, image_filename, IMAGE_FOLDER)
-
+        download_image(full_img_link, image_filename, IMAGE_FOLDER)
+        if comments:
+            comments_filename = f"{id}. {title}-comments.txt"
+            save_comments(comments, book_filename, COMMENTS_FOLDER)
 
         print(f'Книга "{title}" сохранена в: "{book_save_path}"')
 
@@ -45,10 +50,19 @@ def check_redirect(response):
 def get_book_context(response):
 
     context = BeautifulSoup(response.text, 'lxml')
+
     title_and_author = context.find('td', class_='ow_px_td').find('h1').text
-    picture_link = context.find("div", class_="bookimage").find("img")["src"]
     title, author = title_and_author.split('::')
-    return title.strip(), author.strip(), picture_link
+    picture_link = context.find("div", class_="bookimage").find("img")["src"]
+
+    comments = []
+    divs = context.find_all("div", class_="texts")
+    for div in divs:
+        comment = div.find("span", class_="black").text
+        comments.append(comment)
+
+
+    return title.strip(), author.strip(), picture_link, comments
 
 
 def download_txt(response, filename, folder='books/'):
@@ -67,6 +81,12 @@ def download_image(img_url, img_filename, folder='images/'):
     with open(image_save_path, "wb") as image:
         image.write(response.content)
     return image_save_path
+
+def save_comments(comments, filename, folder='comments/'):
+    comments_save_path = os.path.join(folder, filename)
+    with open(comments_save_path, 'w') as file:
+           file.write("\n".join(comments))
+
 
 def get_response_and_check(*urls):
     responses = []
