ff6bf8ac204c2307fea9b7843dff0d8cc0832e04
Kit <nikitabit@yandex.ru>
2023-02-07 14:36:23 +0700

    Split books by two columns.

--- render_website.py
+++ render_website.py
@@ -5,12 +5,14 @@ import json
 from parse_tululu_category import JSON_FOLDER
 from pathlib import Path
 from livereload import Server
+from more_itertools import chunked
 
 def main():
 
     rebuild()
     server = Server()
     server.watch('books.json', rebuild)
+    server.watch('template.html', rebuild)
     server.serve(root='.')
 
 def rebuild():
@@ -21,14 +23,17 @@ def rebuild():
         print("Файл не найден")
         sys.exit()
 
+    chunked_books = chunked(books, 2)
+
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
     template = env.get_template('template.html')
     rendered_page = template.render(
-        books=books
+        book_rows=chunked_books
     )
+
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
     print("Site rebuilt")
