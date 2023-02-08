48b0fa384aaa4d901e672c76493b334d83d69d1d
Kit <nikitabit@yandex.ru>
2023-02-08 15:30:12 +0700

    Rename BOOK_CARDS_PER_PAGE Add arg for script to input custom json path Rename chunked_by_page_book_cards Remove print

--- render_website.py
+++ render_website.py
@@ -7,48 +7,51 @@ from parse_tululu_category import JSON_FOLDER
 from pathlib import Path
 from livereload import Server
 from more_itertools import chunked
+import argparse
 
 COLUMNS_COUNT = 2
-BOOKS_PER_PAGE = 10
+BOOK_CARDS_PER_PAGE = 10
 
 
 def main():
-
     rebuild()
     server = Server()
-    server.watch('books.json', rebuild)
+    server.watch(Path(args.json_path)/'books.json', rebuild)
     server.watch('template.html', rebuild)
     server.serve(root='.')
 
 
 def rebuild():
     try:
-        with open(Path(JSON_FOLDER)/'books.json', "r") as file:
+        with open(Path(args.json_path)/'books.json', "r") as file:
             books = json.load(file)
     except IOError:
         print("Файл не найден.")
         sys.exit()
 
-    chunked_by_page_books = list(chunked(books, BOOKS_PER_PAGE))
+    chunked_by_page_book_cards = list(chunked(books, BOOK_CARDS_PER_PAGE))
     os.makedirs('./pages/', exist_ok=True)
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
-    for count, page in enumerate(chunked_by_page_books, 1):
+    for count, page in enumerate(chunked_by_page_book_cards, 1):
         chunked_by_column_books = chunked(page, COLUMNS_COUNT)
         template = env.get_template('template.html')
         rendered_page = template.render(
             book_rows=chunked_by_column_books,
-            pages_count=len(chunked_by_page_books)+1,
+            pages_count=len(chunked_by_page_book_cards)+1,
             active=count,
             next=count+1,
             previous=count-1,
         )
         with open(f'pages/index{count}.html', 'w', encoding="utf8") as file:
             file.write(rendered_page)
-    print("Site rebuilt")
 
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description="Скрипт генерирует страницы сайта")
+    parser.add_argument('json_path', nargs='?', default=JSON_FOLDER, help='Путь к файлу с данными')
+    args = parser.parse_args()
     main()
